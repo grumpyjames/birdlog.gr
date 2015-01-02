9baf8cca433cd3cb7278b2bee3a4ba529dd84c8e
James Byatt <grumpyjames@gmail.com>
2015-01-02 15:09:10 +0000

    tidyup, switch to tuples in a few more places

--- Cell.elm
+++ Cell.elm
@@ -11,7 +11,7 @@ import Window
 main : Signal Element
 main = S.map2 clg Window.dimensions movement
 
-type alias Tile = { x: Int, y: Int, xpos: Float, ypos: Float}
+type alias Tile = { point: (Int, Int), position: (Float, Float) }
 
 clg : (Int, Int) -> (Int, Int) -> Element
 clg (winx, winy) shift = 
@@ -24,16 +24,20 @@ sgn a = if a > 0 then 1 else (if a < 0 then -1 else 0)
 
 ttf : Int -> Tile -> Form
 ttf size t = 
-    let content = toString t.x ++ "," ++ toString t.y ++ "\n" ++ toString t.xpos ++ "," ++ toString t.ypos
-    in move (t.xpos, t.ypos) <| toForm <| tileEl size content
+    let content = toString t.point ++ "\n" ++ toString t.position
+    in move t.position <| toForm <| tileEl size content
+
+mapT : (a -> b) -> (a, a) -> (b, b)
+mapT f (a1, a2) = (f a1, f a2)
+
+mergeT : (a -> a -> b) -> (a, a) -> (a, a) -> (b, b)
+mergeT op (x1, y1) (x2, y2) = (op x1 x2, op y1 y2) 
 
 step : Int -> Tile -> (Int, Int) -> Tile
-step size tileOff (x, y) = 
-    let xpos = tileOff.xpos + (toFloat (x * size))
-        ypos = tileOff.ypos + (toFloat (y * size))
-        newX = tileOff.x + x
-        newY = tileOff.y + y
-    in Tile newX newY xpos ypos
+step size offsetTile baseCoordinate = 
+    let basePosition = mapT (toFloat << ((*) size)) baseCoordinate
+        addT = mergeT (+)
+    in Tile (addT offsetTile.point baseCoordinate) (addT offsetTile.position basePosition) 
 
 tileEl : Int -> String -> Element
 tileEl sz s = color grey (container sz sz middle (plainText s))
@@ -42,19 +46,12 @@ offset : Int -> (Int, Int) -> Tile
 offset tileSize (x, y)  = 
     let wraps t = (0 - t) // tileSize
         pos t = toFloat <| t % tileSize
-    in Tile (wraps x) (wraps y) (pos x) (pos y) 
+    in Tile (wraps x, wraps y) (pos x, pos y)
 
 coords : Int -> Int -> List (Int, Int)
 coords xc yc = 
-    let xzeroth = ((xc - 1) // 2)
-        minx = 0 - xzeroth
-        maxx = xzeroth
-        yzeroth = ((yc - 1) // 2)
-        miny = 0 - yzeroth
-        maxy = yzeroth
-        xcoords = [minx..maxx]
-        ycoords = [miny..maxy]
-    in cartesianProduct xcoords ycoords
+    let coordRange t = [(( 1 - t ) // 2)..((t - 1) // 2)]
+    in cartesianProduct (coordRange xc) (coordRange yc)
 
 cartesianProduct : List a -> List b -> List (a, b)
 cartesianProduct xs ys = 
@@ -63,7 +60,6 @@ cartesianProduct xs ys =
       [] -> []
 
 -- simplified drags
-
 movement : Signal (Int, Int)
 movement = let rawSignal = S.foldp step' (MouseUp, (0, 0)) pnWhenDown
            in S.map snd <| rawSignal
