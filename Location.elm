8d01b2832b419bcb5e99f18acb08241f9bba10cd
James Byatt <grumpyjames@gmail.com>
2015-01-24 23:18:41 +0000

    show an appropriate tile when a location is received

--- Location.elm
+++ Location.elm
@@ -1,7 +1,16 @@
+import Graphics.Element (Element)
 import Native.Location (location)
+import Osm (centeredOn)
 import Signal (map)
 import Text (..)
 
-main = map (\l -> centered <| fromString <| toString l) Native.Location.location
+main = map show Native.Location.location
+
+show : LocationResponse -> Element
+show lr =
+    case lr of
+      NoneYet -> plainText "No location yet"
+      Error code -> plainText <| "An error of code: " ++ (toString code)
+      LatLn lat lon -> centeredOn 5 lat lon
 
 type LocationResponse = NoneYet | LatLn Float Float | Error Int
--- Osm.elm
+++ Osm.elm
@@ -1,10 +1,20 @@
-module Osm (osm) where
+module Osm (centeredOn, osm) where
 
 import Graphics.Element (Element, image)
 import Tile (..)
 
+-- FIXME: primitive obsession!
+centeredOn : Int -> Float -> Float -> Element
+centeredOn zoom lat lon = 
+    let tx = lon2tilex lon zoom
+        ty = lat2tiley lat zoom
+    in image 256 256 <| osmUrl2 zoom (tx, ty)
+
 osm : Render
 osm size tile = image size size <| osmUrl 5 tile.point
+
+osmUrl2 : Int -> (Int, Int) -> String
+osmUrl2 zoom (x,y) = "http://tile.openstreetmap.org/" ++ (toString zoom) ++ "/" ++ (toString x) ++ "/" ++ (toString y) ++ ".png"
 
 osmUrl : Int -> (Int, Int) -> String
 osmUrl zoom (x,y) = "http://tile.openstreetmap.org/" ++ (toString zoom) ++ "/" ++ (toString (x+10)) ++ "/" ++ (toString ((1-y)+10)) ++ ".png"
@@ -14,6 +24,11 @@ log = logBase e
 tiley2lat y z = 
     let n = pi - 2.0 * pi * y / (2.0 ^ z)
     in 180.0 / pi * atan ( 0.5 * ( (e ^ n) - (e ^ (-n) ) ) )                
-long2tilex lon z = floor((lon + 180.0) / 360.0 * (2.0 ^ z)) 
-lat2tiley lat z = floor((1.0 - log( tan(lat * pi/180.0) + 1.0 / cos(lat * pi/180.0)) / pi) / 2.0 * (2.0 ^ z))
+
+lon2tilex : Float -> Int -> Int
+lon2tilex lon z = floor((lon + 180.0) / 360.0 * (2.0 ^ (toFloat z))) 
+
+lat2tiley : Float -> Int -> Int
+lat2tiley lat z = floor ((1.0 - log( tan(lat * pi/180.0) + 1.0 / cos(lat * pi/180.0)) / pi) / 2.0 * (2.0 ^ (toFloat z)))
+
 tilex2long x z = x / (2.0 ^ z) * 360.0 - 180
