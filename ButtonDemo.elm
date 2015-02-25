10b04e8130f9133051669998f019784a0d42fa92
James Byatt <grumpyjames@gmail.com>
2015-02-25 20:46:27 +0000

    really dreadful buttons

--- ButtonDemo.elm
+++ ButtonDemo.elm
@@ -1,16 +1,30 @@
 module ButtonDemo (main) where
 
-import Graphics.Element (Element, flow, right)
-import Graphics.Input (button)
-import Signal (Channel, Signal, channel, foldp, map, send, subscribe)
+import Color (rgb)
+import Graphics.Element (Element, color, container, flow, layers, middle, right, spacer)
+import Graphics.Input (button, customButton)
+import Signal (Channel, Message, Signal, channel, foldp, map, send, subscribe)
 import Text (plainText)
 
 type ZoomChange = In | Out | None
 
 zoomChange = channel None
 
-zoomIn = button (send zoomChange In) "+"
-zoomOut = button (send zoomChange Out) "-"
+hoverC = rgb 133 133 133
+downC = rgb 99 99 99
+upC = rgb 111 111 111
+
+ourButton : Message -> String -> Element
+ourButton msg txt = 
+    let el = plainText txt
+        cn = layers [container 30 30 middle el, spacer 30 30]
+        up = color upC cn
+        down = color downC cn
+        hover = color hoverC cn
+    in customButton msg up hover down
+
+zoomIn = ourButton (send zoomChange In) "+"
+zoomOut = ourButton (send zoomChange Out) "-"
 
 main : Signal Element
 main = 
