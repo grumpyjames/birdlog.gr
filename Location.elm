e6b299317696c0c72ae10babdf985dffbd7da655
James Byatt <grumpyjames@gmail.com>
2015-05-06 22:02:48 +0100

    Fix location demo, check in elm package json as it's the thing that makes native modules work with reactor

--- Location.elm
+++ Location.elm
@@ -1,27 +1,26 @@
-import Color (red)
-import Graphics.Element (Element, color, container, empty, layers, opacity, topLeft)
-import Native.Location (location)
-import Osm (openStreetMap, simpleOsm)
-import Signal (map)
-import Text (plainText)
-import Tuple (mapT)
-import Types (GeoPoint, Zoom(..))
+import Color exposing (red)
+import Graphics.Element exposing (Element, color, container, empty, layers, opacity, show, topLeft)
+import Native.Location exposing (location)
+import Osm exposing (openStreetMap, simpleOsm)
+import Signal exposing (map)
+import Text exposing (fromString)
+import Types exposing (GeoPoint, Zoom(..))
 
 location = Native.Location.location
 
-main = map show location
+main = map shw location
 
-show : LocationResponse -> Element
-show lr =
+shw : LocationResponse -> Element
+shw lr =
     case lr of
-      NoneYet -> plainText "No location yet"
-      Error code -> plainText <| "An error of code: " ++ (toString code)
+      NoneYet -> show "No location yet"
+      Error code -> show <| "An error of code: " ++ (toString code)
       LatLn lat lon -> showLocation (Zoom 16) {lat = lat, lon = lon}
 
 showLocation : Zoom -> GeoPoint -> Element
 showLocation zoom geopt =
     let offsets = openStreetMap.locate zoom geopt
-        tileImg = simpleOsm zoom <| offsets.tile.coordinate
+        tileImg = simpleOsm zoom <| offsets.tile
         pixelOffsets = offsets.position.pixels
         indicator = container (fst pixelOffsets) (snd pixelOffsets) topLeft empty
     in layers <| [ tileImg, opacity 0.3 <| color red <| indicator ] 
