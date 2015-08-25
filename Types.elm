a79be201325f8c0a1c465070e70aefcfce8ee6b5
James Byatt <grumpyjames@gmail.com>
2015-08-25 18:10:04 +0100

    obey style guide a little better

--- Types.elm
+++ Types.elm
@@ -13,18 +13,40 @@ module Types (GeoPoint,
 import Time exposing (Time)
 
 type alias Zoom = Float
-type alias GeoPoint = { lat: Float, lon: Float }
-type alias Tile = { coordinate : (Int, Int) }
-type alias Position = { pixels : (Int, Int) }
-type alias TileOffset = { tile: Tile, position: Position }
+
+type alias GeoPoint = 
+    { 
+      lat: Float
+    , lon: Float 
+    }
+
+type alias Tile = 
+    { 
+      coordinate : (Int, Int)
+    }
+
+type alias Position = 
+    { 
+      pixels : (Int, Int)
+    }
+
+type alias TileOffset = 
+    { 
+      tile: Tile
+    , position: Position
+    }
+
 type alias Hdpi = Bool
 type alias Locator = Zoom -> GeoPoint -> TileOffset
 type alias TileUrl = Zoom -> Tile -> String
-type alias TileSource = {
-      tileSize : Int,
-      locate: Locator,
-      tileUrl: TileUrl
-}
+
+type alias TileSource =
+    {
+      tileSize : Int
+    , locate: Locator
+    , tileUrl: TileUrl
+    }
+
 type alias Sighting =
     {
       count: Result String Int
@@ -33,13 +55,15 @@ type alias Sighting =
     -- millis
     , time : Time
     }
-type alias Model = {
-      hdpi : Bool,
-      centre : GeoPoint,
-      zoom : Zoom,
-      mouseState : (Bool, (Int, Int)),
-      tileSource : TileSource,
-      clicked : (Time, Maybe (Int, Int)),
-      sighting : Sighting,
-      progress : Bool
-}
+
+type alias Model = 
+    { 
+      hdpi : Bool
+    , centre : GeoPoint
+    , zoom : Zoom
+    , mouseState : (Bool, (Int, Int))
+    , tileSource : TileSource
+    , clicked : (Time, Maybe (Int, Int))
+    , sighting : Sighting
+    , progress : Bool
+    }
