9f5c0e56b879e15584b41dfcbc990d1533ccecbc
James Byatt <grumpyjames@gmail.com>
2015-10-26 21:03:19 +0000

    given a -> JE.Value, can derive Sequenced (Recording a) -> JE.Value

--- src/elm/Replication.elm
+++ src/elm/Replication.elm
@@ -1,7 +1,7 @@
 module Replication (postRecords) where
 
 import Model exposing (Events(..), Sighting)
-import Sequenced exposing (Recording(..), Sequenced)
+import Sequenced exposing (Recording(..), Sequenced, encoder)
 import Types exposing (GeoPoint)
 
 import Http
@@ -24,33 +24,10 @@ postRecords addr rs =
 
 pack : List (Sequenced (Recording Sighting)) -> ReplicationPacket
 pack rs = 
-    let encode rs = Http.string <| JS.encode 0 <| JS.list <| L.map asJson rs 
+    let encode rs = Http.string <| JS.encode 0 <| JS.list <| L.map (encoder sghtJson) rs 
     in case rs of
       [] -> ReplicationPacket -1 Http.empty
       (x :: xs) -> ReplicationPacket x.sequence (encode rs)
-
-asJson : (Sequenced (Recording Sighting)) -> JS.Value
-asJson r = 
-    let nora typ seq item =
-        JS.object [ ("sequence", (JS.int seq))
-                  , ("type", typ)
-                  , ("item", item) 
-                  ]
-    in case r.item of 
-      New s -> 
-          nora typeNew r.sequence <| sghtJson s
-      Replace refSeq s -> 
-          nora (typeRef "Replace" refSeq) r.sequence <| sghtJson s
-      Delete refSeq -> 
-          nora (typeRef "Delete" refSeq) r.sequence <| JS.object []
-
-typeNew : JS.Value
-typeNew = JS.object [ ("name", JS.string "New") ]
-
-typeRef : String -> Int -> JS.Value
-typeRef n seq = JS.object [ ("name", JS.string n)
-                          , ("refersTo", JS.object [("sequence", JS.int seq)])
-                          ]
 
 sghtJson : Sighting -> JS.Value
 sghtJson s = 
--- src/elm/Sequenced.elm
+++ src/elm/Sequenced.elm
@@ -1,10 +1,12 @@
 module Sequenced ( Recording(..)
                  , Sequenced
                  , consolidate
+                 , encoder
                  , fold
                  , matches ) where
     
 import Dict as D
+import Json.Encode as JE
 import List as L
 
 type alias Sequenced a = 
@@ -24,6 +26,30 @@ consolidate rs =
           Replace seq s -> D.insert r.sequence (Sequenced r.sequence s) <| D.remove seq d
           Delete seq -> D.remove seq d
     in D.values <| L.foldr f D.empty rs
+
+encoder : (a -> JE.Value) -> Sequenced (Recording a) -> JE.Value
+encoder valEnc s =
+    let nora typ seq item =
+        JE.object [ ("sequence", (JE.int seq))
+                  , ("type", typ)
+                  , ("item", item) 
+                  ]
+    in case s.item of 
+      New n -> 
+          nora typeNew s.sequence <| valEnc n
+      Replace refSeq r -> 
+          nora (typeRef "Replace" refSeq) s.sequence <| valEnc r
+      Delete refSeq -> 
+          nora (typeRef "Delete" refSeq) s.sequence <| JE.object []
+
+typeNew : JE.Value
+typeNew = JE.object [ ("name", JE.string "New") ]
+
+typeRef : String -> Int -> JE.Value
+typeRef n seq = JE.object [ ("name", JE.string n)
+                          , ("refersTo", JE.object [("sequence", JE.int seq)])
+                          ]
+
 
 fold : (Int -> a -> b) -> (Int -> Int -> a -> b) -> (Int -> Int -> b) -> (Sequenced (Recording a)) -> b
 fold onNew onReplace onDelete s =
